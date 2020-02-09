8d3cdd1c929d3acb9b9cc013c9606b38292cd1c1
adamleo <adamkingleo@gmail.com>
2020-02-09 16:58:12 +0800

    updated file utils

--- fileUtils.go
+++ fileUtils.go
@@ -6,8 +6,7 @@ import (
 )
 
 func GetDirFileLine() (dir, file string, line int) {
-	var pc uintptr
-	pc, file, line, _ = runtime.Caller(1) // using 1 to log where the error happened
+	_, file, line, _ = runtime.Caller(1) // using 1 to log where the error happened
 	dir, file = filepath.Split(file)
 	return
 }
